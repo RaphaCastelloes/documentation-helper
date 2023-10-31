91b86b17fc655a8abede272d617509353429dc72
Raphael Castellões <castelloesjf@hotmail.com>
2023-10-31 11:04:50 -0300

    Ingestion ready

--- ingestion.py
+++ ingestion.py
@@ -2,21 +2,19 @@ import os
 from langchain.document_loaders import ReadTheDocsLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Pinecone
+from langchain.vectorstores.pinecone import Pinecone
 import pinecone
-
-from consts import INDEX_NAME
 
 pinecone.init(
     api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+    environment=os.environ["PINECONE_ENVIRONMENT"],
 )
 
 
 def ingest_docs() -> None:
-    loader = ReadTheDocsLoader(path="langchain-docs/langchain.readthedocs.io/en/latest")
+    loader = ReadTheDocsLoader(path="langchain-docs/api.python.langchain.com/en/latest/chains", encoding="utf-8", features="html.parser")
     raw_documents = loader.load()
-    print(f"loaded {len(raw_documents) }documents")
+    print(f"loaded {len(raw_documents) } documents")
     text_splitter = RecursiveCharacterTextSplitter(
         chunk_size=1000, chunk_overlap=100, separators=["\n\n", "\n", " ", ""]
     )
